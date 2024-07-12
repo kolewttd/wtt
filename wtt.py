bba66c9b84f5fbc11a0a6d1ca729c7f7a415b2a3
kolewttd <kolewtt@proton.me>
2024-07-12 22:55:17 +0200

    Updated wtt and README.md.

--- wtt.py
+++ wtt.py
@@ -4,35 +4,35 @@ import google.generativeai as genai
 
 def arguments_to_text(*args) -> str:
 
-        text = ""
+    text = ""
 
-        for arg in args:
-                if isinstance(arg, str):
-                        if os.path.isfile(arg):
-                                with open(arg, 'r') as file:
-                                        text += file.read()
-                        else:
-                                text += arg + '\n'
-                else:
-                        raise TypeError("Arguments must be file paths or strings.")
+    for arg in args:
+        if isinstance(arg, str):
+            if os.path.isfile(arg):
+                with open(arg, 'r') as file:
+                    text += file.read()
+            else:
+                 text += arg + '\n'
+        else:
+            raise TypeError("Arguments must be file paths or strings.")
 
-        return text
+    return text
 
 def wtt(*args) -> None:
-        prompt = arguments_to_text(*args)
-        genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-        model = genai.GenerativeModel('gemini-pro')
-        response = model.generate_content(prompt)
-        print(response.text)
+    prompt = arguments_to_text(*args)
+    genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+    print(response.text)
 
-        with open("output", "a") as file:
-                file.write(prompt)
-                file.write(response.text)
+    with open("output", "a") as file:
+        file.write(prompt)
+        file.write(response.text)
 
 if __name__ == "__main__":
-        if len(sys.argv) < 2:
-                print("Usage: python wtt.py <file_path_or_string> [<file_path_or_string> ...]")
-                sys.exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: python wtt.py <file_path_or_string> [<file_path_or_string> ...]")
+        sys.exit(1)
 
-        prompt_list = sys.argv[1:]
-        wtt(*prompt_list)
+    prompt_list = sys.argv[1:]
+    wtt(*prompt_list)
